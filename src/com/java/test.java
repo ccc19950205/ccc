6be3f19f818c67507ea44e58e49d824a5e5d7fda
chenchunchuan <chenchunchuan@chinazyjr.com>
2017-07-06 11:50:02 +0800

    test

---
+++ src/com/java/test.java
@@ -0,0 +1,16 @@
+package com.java;
+
+/**
+ * Created by chenchunchuan on 2017/7/6.
+ */
+public class test {
+    private  int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
